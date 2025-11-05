0d5896a1b11aa9955ac08a298b59f3e01f230a4a
henrique_b_correa <henriquebatistacorreaa2@gmail.com>
2025-11-04 21:44:03 -0300

    ajuste conexão

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,20 +1,6 @@
 const config = {
-  development: {
-    API_URL: 'http://localhost:8069/api',
-    TIMEOUT: 10000,
-  },
-  device: {
-    API_URL: 'http://10.1.140.41:8069/api', 
-    TIMEOUT: 15000,
-  },
+  API_URL: 'http://10.0.2.2:8069/api',
+  TIMEOUT: 10000,
 };
 
-const getConfig = () => {
-  if (__DEV__) {
-    return config.device;
-  }
-  return config.production;
-};
-
-export default getConfig();
-
+export default config;
